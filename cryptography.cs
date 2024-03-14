7b8bce6d6b9c9f0a6ec570f4952664e7662f02bd
PrplDragon1 <113979980+PrplDragon1@users.noreply.github.com>
2024-03-14 10:04:51 +0000

    Update cryptography.cs

--- cryptography.cs
+++ cryptography.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
-namespace WinFormAdminPanel
+namespace EncryptionExample
 {
     public class Cryptography
     {
